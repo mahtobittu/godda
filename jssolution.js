2a36b315136eef9ab622520260352bd05035c940
mahtobittu <mahtobittu@gmail.com>
2018-11-12 18:14:04 +0530

    Add files via upload

--- jssolution.js
+++ jssolution.js
@@ -1,6 +1,20 @@
-//$(document).ready(function () {
+
+var elem = document.getElementById('hamburger');
+var line = document.getElementsByClassName('line');
+var ham  = document.getElementsByClassName('ham')
+var ham1 = document.getElementsByClassName('ak');
 
 
+ elem.addEventListener("click",myFunction,false);
+ ham1[0].addEventListener("click",myFunction,false);
+ ham1[1].addEventListener("click",myFunction,false);
+ ham1[2].addEventListener("click",myFunction,false);
+
+ function myFunction(){
+   line[0].classList.toggle('line1');
+   line[1].classList.toggle('line2');
+   line[2].classList.toggle('line3'); 
+   ham[0].classList.toggle('hamdisplay');    
+ }
 
 
-//});
